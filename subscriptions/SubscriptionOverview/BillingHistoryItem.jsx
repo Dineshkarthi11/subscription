805efb07282c00bbd3bea4eea4647536d27b9ee5
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-11 09:04:16 +0530

    BillingHistoryItem.jsx

--- subscriptions/SubscriptionOverview/BillingHistoryItem.jsx
+++ subscriptions/SubscriptionOverview/BillingHistoryItem.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import Invoice from "../../../../assets/images/Invoice.png";
-import uparrow from "../../../../assets/images/uparrow.png";
-import downarrow from "../../../../assets/images/downarrow.png";
 import detailbg from "../../../../assets/images/detailbg.png";
 
 function BillingHistoryItem({ date, orderId, isLatest, planDetails }) {
@@ -29,38 +26,22 @@ function BillingHistoryItem({ date, orderId, isLatest, planDetails }) {
           <div className="grow my-auto text-sm text-black">
             Order ID: {orderId}
           </div>
-          <div className="flex items-center gap-4">
-            <button
-              className="flex overflow-hidden gap-2 justify-center items-center px-5 py-3.5 text-base bg-white rounded-lg border border-solid shadow-sm border-black border-opacity-10 text-stone-950"
-              onClick={(e) => {
-                e.stopPropagation(); // Prevents click on the card from triggering this button's toggle
-              }}
-            >
-              <img
-                loading="lazy"
-                src={Invoice}
-                alt=""
-                className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square"
-              />
-              Download Invoice
-            </button>
-            <button
-              onClick={(e) => {
-                e.stopPropagation(); // Prevents the entire card's toggle
-                toggleExpand();
-              }}
-            >
-              <img
-                src={
-                  isExpanded
-                    ? "https://github.com/Dineshkarthi11/loyaltri/blob/main/assets/up.png?raw=true"
-                    : "https://github.com/Dineshkarthi11/loyaltri/blob/main/assets/down.png?raw=true"
-                }
-                alt="Toggle"
-                className="w-8"
-              />
-            </button>
-          </div>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleExpand();
+            }}
+          >
+            <img
+              src={
+                isExpanded
+                  ? "https://github.com/Dineshkarthi11/loyaltri/blob/main/assets/up.png?raw=true"
+                  : "https://github.com/Dineshkarthi11/loyaltri/blob/main/assets/down.png?raw=true"
+              }
+              alt="Toggle"
+              className="w-8"
+            />
+          </button>
         </div>
       </div>
       {isLatest && isExpanded && planDetails && (
@@ -76,7 +57,7 @@ function BillingHistoryItem({ date, orderId, isLatest, planDetails }) {
             <div className="relative my-10 overflow-y-auto">
               {planDetails.map((detail, index) => (
                 <div key={index} className="flex my-6 justify-between text-sm mx-6 mt-2">
-                  <div className="truncate ">{detail.label}</div>
+                  <div className="truncate">{detail.label}</div>
                   <div className="truncate mx-[40%]">{detail.value}</div>
                 </div>
               ))}

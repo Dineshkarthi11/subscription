e46e587b194676abd40c44c6d1d77d5c9dc11cca
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-17 09:07:50 +0530

    CouponCodeInput.jsx

--- subscriptions/RequestAdditionalUser/CouponCodeInput.jsx
+++ subscriptions/RequestAdditionalUser/CouponCodeInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import coupon from "../../../../assets/images/coupon.png";
 
-function CouponCodeInput({ closeRequestAdditionalUserModal, onApplyCoupon }) {
+function CouponCodeInput({ onApplyCoupon }) {
   return (
     <div className="flex flex-col mt-3.5 w-[410px] text-sm leading-none min-h-[53px]">
       <div className="flex overflow-hidden gap-10 justify-between items-center px-2 py-2.5 w-full rounded-lg border border-gray-300 border-dashed bg-zinc-50 min-h-[53px]">
@@ -19,7 +19,7 @@ function CouponCodeInput({ closeRequestAdditionalUserModal, onApplyCoupon }) {
           />
         </div>
         <button
-          onClick={onApplyCoupon} // Use the passed apply coupon handler
+          onClick={onApplyCoupon} // Switch to the second page on apply
           className="flex items-start self-stretch my-auto font-semibold whitespace-nowrap rounded-lg text-zinc-800 overflow-hidden gap-2 px-3.5 py-2 bg-white border border-solid shadow-sm border-black border-opacity-10"
         >
           Apply
